309662a3a499ae73feaeee2b0e2466c614f68f59
mcbryan1 <git config --global user.email solomonmcbryan@gmail.comgit config --global user.email>
2020-09-22 16:00:15 +0000

    Couldn't Finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import WeatherTemplate from "./WeatherTemplate";
+import WeatherTemplate fro./WeatherTemplatemplate";
 
 class App extends Component {
   constructor(props) {
